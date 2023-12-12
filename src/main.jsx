d8806591127c24455bbbfd5f205fb24593f1a3f5
Rishueee <134083360+Rishueee@users.noreply.github.com>
2023-12-12 11:10:14 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+import Questans from './App.jsx'
+import ReactDOM from 'react-dom/client'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Questans />
+  </React.StrictMode>,
+)
